fdfc8c6fdc77b68e8fb726e540803fc186cb04d4
weswigham <wwigham@gmail.com>
2014-05-08 17:55:51 -0400

    change color

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
+    center: new google.maps.LatLng(44.5, -0.36), //France-ish
+    zoom: 2
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
                                 mapOptions);
   
+  map.data.setStyle({
+    fillColor: 'red',
+    strokeWeight: 1
+  });
   map.data.loadGeoJson('vineyards.json');
+  
+  map.data.addListener('mouseover', function(event) {
+        console.log(event.feature.getProperty('clc_00')); //Should always be 221 - Vineyard
+  });
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
